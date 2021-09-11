aa3bbc153c460b66b6566a2d7672602c5e861526
irina Siskova <Irina.Siskova@ivkhk.ee>
2021-09-11 12:34:52 +0300

    Третья версия проекта

--- src/vertex/Vertex.java
+++ src/vertex/Vertex.java
@@ -2,47 +2,8 @@ package vertex;
 import java.util.Scanner;   //импорт сканера
 public class Vertex {
 
-    public static void main(String[] args) {
-        //Конкатенация строк
-        /*
-        String studies1 = "учиться очень легко и интересно";
-        String studies2 = "учиться очень трудно, но нужно";
-      
-        System.out.println("Каждому понятно, что " + studies1);
-        System.out.println("Каждому понятно, что " + studies2);
-         System.out.println("Каждому понятно, что " + studies2);
-          System.out.println("Каждому понятно, что " + studies2);
-        */
-        
-        /*Задача1
-        Пользователь должен ввести в консоль любое целое число от 1 до 10.
-        А программа выведет ему ответ: какое число он ввел.
-        
-        System.out.println("Введите любое целое число от 1 до 10: ");   
-        //просим пользователя ввести число
-        Scanner scan = new Scanner(System.in);   //объявляем сканер
-        int number = scan.nextInt();   
-        //метод сканера считывать целые числа(int) с консоли и присвоить его переменной number
-        System.out.println("Вы ввели число: " + number);
-        */
-        
-        /*Задача2
-        Пользователь вводит два целых числаю
-        Нужно вывести в консоль сумму этих двух чисел.
-        
-        
-        System.out.println("Введите в консоль два любых целых числа: ");
-        Scanner one = new Scanner(System.in);
-        int num1 = one.nextInt();
-        int num2 = one.nextInt();
-        System.out.println(num1 + num2);t
-        
-        */
-        
-        /*
-        Ввести несколько любых слов или фраз.
-        */
-        
+    public static void main(String[] args) {      
+        //Ввести несколько любых слов или фраз.
         Scanner two = new Scanner(System.in);
         System.out.println("Введите любое слово или фразу:");
 
